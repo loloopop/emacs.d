221d7f86d48b12c1ee2b68c7a90f30e4d0ebf112
陨落流星 <loloopop@126.com>
2019-04-21 16:17:35 +0800

    2019-04-21 add auto-complete auto-complete-clang auto-yasnippet

---
+++ auto-yasnippet/auto-yasnippet-test.el
@@ -0,0 +1,45 @@
+(require 'ert)
+
+(ert-deftest aya--parse ()
+  (should (equal (aya--parse "public void `Foo'TestMethod(`Type' param1, `Type' param2)")
+                 '("public void "
+                   ((idx . 1)
+                    (value . "foo")
+                    (ucase . t))
+                   "TestMethod("
+                   ((idx . 2)
+                    (value . "type")
+                    (ucase . t))
+                   " param1, "
+                   ((idx . 2)
+                    (value . "type")
+                    (ucase . t))
+                   " param2)")))
+  (should (equal (aya--parse "public void ~FooTestMethod(`Type' param1, `Type' param2)")
+                 '("public void "
+                   ((idx . 1)
+                    (value . "fooTestMethod")
+                    (ucase . t))
+                   "("
+                   ((idx . 2)
+                    (value . "type")
+                    (ucase . t))
+                   " param1, "
+                   ((idx . 2)
+                    (value . "type")
+                    (ucase . t))
+                   " param2)"))))
+
+(ert-deftest aya--maybe-append-newline ()
+  (should (equal (let ((aya-create-with-newline t))
+                   (aya--maybe-append-newline "snippet"))
+                 "snippet\n"))
+  (should (equal (let ((aya-create-with-newline nil))
+                   (aya--maybe-append-newline "snippet"))
+                 "snippet"))
+  (should (equal (let ((aya-create-with-newline t))
+                   (aya--maybe-append-newline "snippet\n"))
+                 "snippet\n"))
+  (should (equal (let ((aya-create-with-newline nil))
+                   (aya--maybe-append-newline "snippet\n"))
+                 "snippet\n")))
